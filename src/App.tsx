d17c0a14a42276f7c2f6d1d917f2d02f873c9f8d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-02 21:50:41 -0300

    feat: add tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
-
   return (
-    <h1>Hello World</h1>
-  )
+    <div>
+      <h1>Hello World</h1>
+      <button>teste</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
